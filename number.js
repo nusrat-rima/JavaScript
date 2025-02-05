9c51642af4f2536c339095ee64d1b14d6fbbe773
nusrat-rima <nusratrima3151@gmail.com>
2025-02-05 23:10:07 +0600

    Fundamentals of JS numbers

---
+++ number.js
@@ -0,0 +1,15 @@
+var orangePrice = 20;
+var chocolate = 0.5;
+var apple = parseInt('22.5');
+var apple = parseFloat('22.5');
+
+// console.log(orangePrice);
+// console.log(apple);
+// console.log(orangePrice + chocolate);
+
+var first = 0.1;
+var second = 0.2;
+var total = first + second;
+//console.log(total.toFixed(2));
+const num = 50 / 0;
+console.log(num);
