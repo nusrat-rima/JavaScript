6b46734784a3646154befa58c5a8626dfa336f1f
nusrat-rima <nusratrima3151@gmail.com>
2025-02-05 23:29:37 +0600

    Mathematical Operation Shorthand

---
+++ shorthand.js
@@ -0,0 +1,16 @@
+var price = 35;
+// price = price + 10;
+price += 10;
+// console.log(price);
+
+var fridge = 50000;
+//fridge -= 1000;
+// fridge *= 3;
+// console.log(fridge);
+
+var first = 'Mobarok';
+var second = 'Tobarok';
+var fullName = first + second;
+// console.log(fullName);
+
+console.log(40+'3');
