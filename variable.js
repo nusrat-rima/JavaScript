b88fc60fd67b189e00fc3f9f48e0eae7a5da500f
nusrat-rima <nusratrima3151@gmail.com>
2025-02-05 21:26:16 +0600

    variable declaration in Js

---
+++ variable.js
@@ -0,0 +1,28 @@
+/*
+ VARIABLE ==> vary +able
+5 things to declare a VARIABLE
+*/
+var weight = 31;
+// var price = 12;
+var year = 2003;
+var money = 10000;
+var age = 12;
+const price = 13;
+// console.log(year);
+// console.log(price);
+
+//String
+var name = 'Fariya';
+var address = 'GPO Laxmipur';
+var country = 'USA';
+//console.log(country);
+
+//boolean
+var isHonest = true;
+//console.log(isHonest);
+
+var passed = false;
+console.log(passed);
+console.log(typeof passed);
+console.log(typeof country);
+console.log(typeof year);
