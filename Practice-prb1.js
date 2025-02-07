a1fe3aee925a64f22075d8c34d4253c8e1c77e11
nusrat-rima <nusratrima3151@gmail.com>
2025-02-07 19:48:17 +0600

    Basic Problems

---
+++ Practice-prb1.js
@@ -0,0 +1,30 @@
+//1.
+// var oranges = 300;
+// var apples = 400;
+// var total = oranges + apples;
+// var remain = 1000 - total;
+// console.log(remain);
+
+//2.
+var mathematics = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.5;
+var total = mathematics + biology + chemistry + physics + bangla;
+var average = total / 5;
+console.log(average.toFixed(2));
+
+//3.
+// var number = 119;
+// var remainder = 119 % 5;
+// console.log(remainder);
+
+//4.
+// * What is onst in JS?
+// Ans : const is a keyword in JavaScript that is used to declare a variable whose value cannot be changed after it is defined.
+
+// const a = isNaN('11');
+// console.log(a);it returns false
+// const b = isNaN(2 - 10);
+// console.log(b); it returns false
