73542e11794240b9534f01a8dae42fb21ae0d30e
nusrat-rima <nusratrima3151@gmail.com>
2025-02-09 22:18:49 +0600

    include,isArray method

---
+++ include.js
@@ -0,0 +1,11 @@
+const numbers = [3, 4, 11, 2, 6, 74];
+console.log(numbers.includes(9));
+console.log(numbers.indexOf(4));
+
+const nums = [];
+const flower = 'rose';
+
+const age = 15;
+console.log(Array.isArray(flower));
+console.log(Array.isArray(nums));
+console.log(Array.isArray(age));
