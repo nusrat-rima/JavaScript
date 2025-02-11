2f32ba2446f321e583aa1ed2ba9fbd432854c3b2
nusrat-rima <nusratrima3151@gmail.com>
2025-02-12 00:49:55 +0600

    object with loops

---
+++ object-loop.js
@@ -0,0 +1,21 @@
+const mobile = {
+  brand: 'samsung',
+  price: 25000,
+  color: 'black',
+  camera: '12mp',
+};
+
+//for of : Array
+// for in : Object
+
+for (const prop in mobile) {
+  //console.log(prop); //all keys
+  //console.log(mobile[prop]);
+}
+
+const keys = Object.keys(mobile);
+console.log(keys);
+
+for (const key of keys) {
+  console.log(key);
+}
