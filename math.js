c03e1aa9677373df76ad526a4fb87bbedb763453
nusrat-rima <nusratrima3151@gmail.com>
2025-02-22 20:24:25 +0600

    Math,Abs,Round,Ceil,Floor and Random Number

---
+++ math.js
@@ -0,0 +1,17 @@
+const min = Math.min(24,2,3,6,99,100)
+console.log(min)
+const max = Math.max(24, 2, 3, 6, 99, 100);
+console.log(max)
+
+
+console.log(Math.PI);
+console.log(Math.abs(313-293))
+console.log(Math.round(4.5))//->5
+console.log(Math.floor(4.5))//->4
+console.log(Math.ceil(4.5))//->5
+
+
+console.log(Math.random())
+console.log(Math.random()*10)
+const rand = Math.round(Math.random()*10)
+console.log(rand)
